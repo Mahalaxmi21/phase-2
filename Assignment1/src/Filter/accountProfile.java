555eae2424d7982c4c518432652eefd137353a0c
Mahalaxmi21 <Mahalaxmihavalad21@gmail.com>
2022-02-20 23:58:02 +0530

    Filter

---
+++ Assignment1/src/Filter/accountProfile.java
@@ -0,0 +1,33 @@
+package Filter;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class accountProfile
+ */
+public class accountProfile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public accountProfile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().write("I am in Account Profile after passing through LoginFilter");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
